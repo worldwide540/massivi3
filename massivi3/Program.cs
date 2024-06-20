2f4ff1cd28727b3da59f486284839e508c6732ae
Нарек Антонян <narekantonyan2033@gmail.com>
2024-06-20 18:38:56 +0300

    Добавьте файлы проекта.

---
+++ massivi3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 5, 10, 15, 20, 25 };
+
+        int sum = 0;
+        foreach (int num in array)
+        {
+            sum += num;
+        }
+
+        double average = (double)sum / array.Length;
+
+        Console.WriteLine($"Сумма элементов массива: {sum}");
+        Console.WriteLine($"Среднее арифметическое значение элементов массива: {average}");
+    }
+}
